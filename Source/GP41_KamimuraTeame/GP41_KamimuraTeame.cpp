fe137434cfadfd5553960782fa84879c52b6d67c
MK413g <mkou7504mmko@outlook.jp>
2023-09-08 18:55:57 +0900

    [add]Player

---
+++ Source/GP41_KamimuraTeame/GP41_KamimuraTeame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GP41_KamimuraTeame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GP41_KamimuraTeame, "GP41_KamimuraTeame" );
