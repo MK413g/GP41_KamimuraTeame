4c6c94be87805d8a9e8c62e1332be65241d683f3
MK413g <91109132+MK413g@users.noreply.github.com>
2023-10-23 10:50:16 +0900

    [update]音量調整追加 BPで音量を調整できるように修正

---
+++ Source/GP41_KamimuraTeame/Manager/MainGameEndManager/MainGameEndManager.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainGameEndManager.h"
+
+E_MainGameEndStat AMainGameEndManager::stat = E_MainGameEndStat::Null;
+bool AMainGameEndManager::gameclear = false;
+bool AMainGameEndManager::gameover = false;
+
+// Sets default values
+AMainGameEndManager::AMainGameEndManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	gameover = false;
+	gameclear = false;
+
+}
+
+// Called when the game starts or when spawned
+void AMainGameEndManager::BeginPlay()
+{
+	Super::BeginPlay();
+	EnableInput(GetWorld()->GetFirstPlayerController());
+	stat = E_MainGameEndStat::Null;
+	gameover = false;
+	gameclear = false;
+}
+
+void AMainGameEndManager::SetOver()
+{
+	if (stat == E_MainGameEndStat::Null) {
+		stat = E_MainGameEndStat::Over;
+	}
+}
+
+void AMainGameEndManager::SetClear()
+{
+	if (stat == E_MainGameEndStat::Null) {
+		stat = E_MainGameEndStat::Clear;
+	}
+}
+
+void AMainGameEndManager::ExecEnumOutput(E_MainGameEndStat st, E_MainGameEndStat& DebugTestEnumOutput)
+{
+	DebugTestEnumOutput = st;
+}
+
+E_MainGameEndStat AMainGameEndManager::GetStat()
+{
+	return stat;
+}
+
+bool AMainGameEndManager::GetGameEnd()
+{
+	return stat != E_MainGameEndStat::Null;
+}
+
+bool AMainGameEndManager::GetGameClearFlg()
+{
+	return gameclear;
+}
+
+bool AMainGameEndManager::GetGameOverFlg()
+{
+	return gameover;
+}
+
+void AMainGameEndManager::SetOverStat_Implementation()
+{
+	stat = E_MainGameEndStat::End;
+	gameover = true;
+}
+
+void AMainGameEndManager::SetClearStat_Implementation()
+{
+	stat = E_MainGameEndStat::End;
+	gameclear = true;
+}
+
+
